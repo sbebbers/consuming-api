cdcba601a2fb51c09d1107d6573230c27851baa4
Shaun Bebbington <shaun@retrogames.biz>
2021-01-27 22:33:48 +0000

    Interim commit
    Does a sanity check to ensure that the pre-requisite file is present

--- public_html/index.php
+++ public_html/index.php
@@ -1,10 +1,18 @@
 <?php
 define('APPLICATION', 0xc0ffee);
+define('CREDENTIALS' , 'credentials-test.json');
 
 require_once ('./../application/core/helpers.php');
 
-writeToErrorLog([
-    'TEST ' . time()
-], JSON_PRETTY_PRINT);
+$credentialPath = getApplicationPath('/auth/', CREDENTIALS);
+
+if(! file_exists($credentialPath)){
+    http_response_code(500);
+    $error = ['File missing: ' . CREDENTIALS];
+    
+    require_once(getApplicationPath(NULL, '/view/error-response.php'));
+}
+
+$credentials = json_decode(file_get_contents($credentialPath));
 
 exit();
